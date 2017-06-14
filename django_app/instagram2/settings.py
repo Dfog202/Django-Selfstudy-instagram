5b1a0ed71af7838c96f3bbec43842c9f5ec0465f
Seungha Lee <poopa8788@gmail.com>
2017-06-14 11:08:27 +0900

    post app 추가, models 내용 추가

--- django_app/instagram2/settings.py
+++ django_app/instagram2/settings.py
@@ -17,6 +17,17 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
 
+# CUSTOM USER
+AUTH_USER_MODEL = 'member.User'
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.11/howto/static-files/
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    STATIC_DIR,
+]
+
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
@@ -39,6 +50,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'member',
+    'post',
     'django_extensions',
 ]
 
@@ -119,10 +131,3 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.11/howto/static-files/
-
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    STATIC_DIR,
-]
--- django_app/member/models.py
+++ django_app/member/models.py
@@ -1,3 +1,8 @@
+from django.contrib.auth import get_user_model
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
+
+class User(AbstractUser):
+    pass
 
