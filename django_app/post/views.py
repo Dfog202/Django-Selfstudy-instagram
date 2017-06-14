30af900500f8e42681fe4f9768af40532f23b757
Seungha Lee <poopa8788@gmail.com>
2017-06-14 14:30:52 +0900

    delete 버튼 생성

--- django_app/post/views.py
+++ django_app/post/views.py
@@ -1,11 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Post
 
 
 def post_list(request):
-    posts = Post.objects.order_by('-created_time')
+    post = Post.objects.order_by('-created_time')
+    # post = Post.objects.all()
     context = {
-        'posts': posts
+        'posts': post
     }
     return render(request, 'post/post_list.html', context)
+
+
+def post_delete(request, post_pk):
+    if request.method == 'POST':
+        post = Post.objects.get(pk=post_pk)
+        post.delete()
+
+    return redirect('post_list')
